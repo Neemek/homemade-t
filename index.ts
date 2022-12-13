2759d6eb4d1013936ec903b50de250d076250368
Neemek <sindodeg@icloud.com>
2022-12-13 19:55:05 +0100

    T factory

--- index.ts
+++ index.ts
@@ -1,37 +1,76 @@
 import { readFileSync } from "fs";
 
-interface tFunction {
-  (key: string, lang?: string): string;
+interface TOptions {
+	onlySingleSpaces?: boolean;
+}
+
+interface TFunction {
+	(key: string, keymap?: object, options?: TOptions): string;
+}
+
+interface LocaleSetter {
+	(key);
 }
 
 class makeT {
-  fromJSON(data: object): tFunction | undefined {
-    const lookup = new Map<string, Map<string, string>>();
+	fromJSON(data: object): [TFunction, LocaleSetter] | undefined {
+		const lookup = new Map<string, Map<string, string>>();
 
-    try {
-      for (let localizationKey of Object.keys(data)) {
-        const localization = new Map<string, string>();
-        const localizationData = data[localizationKey];
-        for (let valueKey of Object.keys(localizationData)) {
-          localization.set(valueKey, localizationData[valueKey]);
-        }
+		let locales = [];
+		try {
+			locales = Object.keys(data);
+			if (locales.length === 0) throw new Error("Empty JSON object.");
 
-        lookup.set(localizationKey, localization);
-      }
-    } catch {
-      return undefined;
-    }
+			for (let locale of locales) {
+				const localization = new Map<string, string>();
+				const localizationData = data[locale];
+				for (let valueKey of Object.keys(localizationData)) {
+					localization.set(valueKey, localizationData[valueKey]);
+				}
 
-    return (key: string, lang: string = "en"): string => {
-      return lookup.get(lang)?.get(key) ?? "";
-    };
-  }
+				lookup.set(locale, localization);
+			}
+		} catch {
+			return undefined;
+		}
+		let locale = locales.includes("en") ? "en" : locales.at(0);
 
-  fromFile(filePath: string) {
-    let data = JSON.parse(readFileSync(filePath).toString());
+		return [
+			(
+				key: string,
+				keymap: object = {},
+				options: TOptions = { onlySingleSpaces: true }
+			): string => {
+				let value = this.replaceWithKeys(lookup.get(locale)?.get(key), keymap);
+				if (value === undefined) return key;
 
-    this.fromJSON(data);
-  }
+				if (options.onlySingleSpaces) return value?.replace(/ {2,}/g, " ");
+				else return value;
+			},
+			(newLocale: string): void => {
+				locale = newLocale;
+			},
+		];
+	}
+
+	fromFile(filePath: string) {
+		let data = JSON.parse(readFileSync(filePath).toString());
+
+		this.fromJSON(data);
+	}
+
+	private replaceWithKeys(
+		text: string | undefined,
+		keymap: object = {}
+	): string | undefined {
+		if (text === undefined || text === "") return undefined;
+
+		for (let key in Object.keys(keymap)) {
+			text = text.replace(`{{${key}}}`, keymap[key]);
+		}
+
+		return text;
+	}
 }
 
-export { makeT };
+export { makeT as default, TFunction, TOptions, LocaleSetter };
